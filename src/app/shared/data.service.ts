1efcdea62e406894908a4f26c33aa7b4a7744559
KevinYang <chgc_kevin@hotmail.com>
2016-07-13 22:19:27 +0800

    change data.service into rxjs way

--- src/app/shared/data.service.ts
+++ src/app/shared/data.service.ts
@@ -1,25 +1,26 @@
 import { Injectable } from '@angular/core';
 import { Http, Request } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Injectable()
 export class DataService {
 
   url = '/api/articles.json';
 
-  constructor(private http: Http) {}
+  constructor(private http: Http) { }
 
   searchArticles(keyword: string) {
     return this.http.get(this.url)
-      .map(res => {
-        return res.json().filter(value => {
-          if(keyword) {
-            return value.title.toLowerCase().indexOf(keyword.toLowerCase()) >= 0;
-          }
-          else {
-            return true;
-          }
-        });
-      });
+      .flatMap(res => {
+        return Observable.from(res.json());
+      })
+      .filter((x: any) => {
+        if (keyword) {
+          return x.title.toLowerCase().indexOf(keyword.toLowerCase()) >= 0;
+        } else {
+          return true;
+        }
+      }).toArray();
   }
 }
